67c8d3bd65f785edf309b653a7611f244ba1ae6f
Lourine <lourine.millicent@gmail.com>
2020-10-30 18:03:57 +0300

    Add deployed base url

--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'https://apitutorial.herokuapp.com/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
